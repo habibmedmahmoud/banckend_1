f383d00314d47cf4726ae8877f1f0aaf4caecd8f
BRAHIM SALEM <habibmedmahmoud5@gmail.com>
2024-12-01 21:53:35 +0000

    mise ja de controller de delivery

--- routes/deliveryRoutes.js
+++ routes/deliveryRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const deliveryController = require('../controllers/deliveryController');
-
+// auth 
 // تسجيل
 router.post('/signup', deliveryController.signup);
 
@@ -11,7 +11,7 @@ router.post('/login', deliveryController.login);
 // Route de vérification du code de vérification
 router.post('/verifycode', deliveryController.verifyCode); // Utiliser la fonction verifyCode directement
 
-
+// forgetpassword 
 // Route pour vérifier l'email et envoyer un code de vérification
 router.post('/check-email', deliveryController.checkEmail);
 router.post('/checkverifycode', deliveryController.checkVerifyCode);
@@ -19,12 +19,27 @@ router.post('/reset-password', deliveryController.resetPassword);
 router.post('/resend-verify-code', deliveryController.resendVerifyCode);
 
 
-// Route GET avec un paramètre deliveryId
+// orders
 
 // Define the route to get filtered orders by delivery ID
-// router.get('/orders/delivery/:id', deliveryController.getFilteredOrders);
-
+// accepted  
+router.get('/accepted/delivery/:id', deliveryController.getFilteredOrders);
+// approve 
 router.post('/approve-order', deliveryController.approveOrder);
+
+// Route pour récupérer les commandes par livreur et statut
+router.get('/archive/delivery/:id', deliveryController.fetchOrdersForDelivery);
+// Route pour récupérer les détails d'une commande
+//details 
+router.get('/details/:id', deliveryController.fetchOrderDetails); 
+// done 
+// Route pour mettre à jour le statut de la commande et envoyer les notifications
+router.post('/update-order', deliveryController.updateOrderStatusAndNotify);
+// // pending 
+// Route pour récupérer les commandes avec les adresses
+router.get('/orders', deliveryController.getOrdersWithAddress);
+
+
 
 
 module.exports = router;
